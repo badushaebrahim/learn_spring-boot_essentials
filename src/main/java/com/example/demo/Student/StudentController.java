4dd06e81327780f914ffada876fa54b2d7d29dbd
badushaebrahim <bbadusha13@gmail.com>
2022-09-08 20:47:31 +0530

    init

---
+++ src/main/java/com/example/demo/Student/StudentController.java
@@ -0,0 +1,24 @@
+package com.example.demo.Student;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/v1/stud")
+public class StudentController {
+	private final StudentService studentService;
+
+	public StudentController(StudentService studentservice){
+		this.studentService = new StudentService();
+	}
+	@GetMapping
+	public List<Student> getStudents(){
+		return studentService.getStudents();
+		
+	}
+}
---
+++ src/main/java/com/example/demo/Student/StudentService.java
@@ -0,0 +1,11 @@
+package com.example.demo.Student;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+public class StudentService {
+	public List<Student> getStudents(){
+		return List.of( new Student(12L,"badu2", "b@b.com", LocalDate.of(2000,Month.JANUARY,4), 21));	
+	}
+}
