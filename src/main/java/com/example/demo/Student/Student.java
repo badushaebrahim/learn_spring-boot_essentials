7ab1f924757301452deffec7a44a46ad58a32f7a
badushaebrahim <bbadusha13@gmail.com>
2022-09-10 21:13:19 +0530

    postgres bb conected , model made

--- src/main/java/com/example/demo/Student/Student.java
+++ src/main/java/com/example/demo/Student/Student.java
@@ -2,7 +2,25 @@ package com.example.demo.Student;
 
 import java.time.LocalDate;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+@Entity
+@Table
 public class Student {
+	@Id
+	@SequenceGenerator(
+		name = "student_squence",
+		sequenceName = "student_squence",
+		allocationSize = 1
+	)
+	@GeneratedValue(
+		strategy = GenerationType.SEQUENCE,
+		generator = "student_squence"
+	)
 	private Long id;
 	private String name;
 	private String email;
